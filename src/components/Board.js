987a31b252a60c875fc79f81aaeccfb01a4a1d9d
Veronika <drymacboa@gmail.com>
2026-02-03 00:10:50 +0100

    Render snake on board using React state

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,5 +1,21 @@
-const Board = () => {
-  return <div className="board"></div>;
-};
+import { useState } from "react";
 
+const Board = () => {
+  const [snake, setSnake] = useState([{ x: 10, y: 10 }]);
+
+  return (
+    <div className="board">
+      {snake.map((segment, index) => (
+        <div
+          key={index}
+          className="snake"
+          style={{
+            gridColumn: segment.x,
+            gridRow: segment.y
+          }}
+        />
+      ))}
+    </div>
+  );
+};
 export default Board;
