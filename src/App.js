788c96b1860d6d6b0c45c5590b74ff0470c279d7
Veronika <drymacboa@gmail.com>
2026-02-02 21:16:23 +0100

    Create game board using CSS Grid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import logo from "./logo.svg";
+import Board from "./components/Board";
 import "./App.css";
 
 function App() {
-  return <h1>Snake Game</h1>;
+  return (
+    <div className="app">
+      ]<h1>Snake Game</h1>
+      <Board />
+    </div>
+  );
 }
 
 export default App;
